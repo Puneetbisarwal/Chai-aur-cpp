4b6b2acbf3ec0377e74cfbe435746425aad36e6d
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-11-10 18:22:27 +0530

    my first code in cpp

---
+++ 01_varconst/hello1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+namespace myChai
+{
+  void display ()
+  {
+    cout << " Hello this is display section." << endl ;
+  }
+}
+
+int main()
+{
+ myChai::display();
+ cout << " My name is Puneet \n Hello Chai and cpp from Puneet." << endl ;
+
+ return 0;
+}
