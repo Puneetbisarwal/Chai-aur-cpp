5677c06840f4471f6f9c6801bb04ae7092cae075
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-11-12 13:08:01 +0530

    push correct folder structure

---
+++ 02_primitives/myString1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string favoriteTea = "Lemon Tea \t";
+    //char favoriteTea1 = "Lemon Tea";
+
+    string description = "Known as \"best\" tea";
+
+    cout << " Favorite Tea = " << favoriteTea << description << endl ;
+    cout << " Favorite Tea = " << favoriteTea <<  endl ;
+    cout << " Description = " << description << endl ;
+
+    return 0;
+}
