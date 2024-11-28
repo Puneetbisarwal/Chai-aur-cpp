d54f252107659e30dcfec91306db41ea02b943f1
Puneetbisarwal <puneetbisarwal@gmail.com>
2024-11-28 18:40:13 +0530

    challenge1.md Question- Challenge 3 Data types and challenges in C++

--- 02_primitives/Challenge_3.cpp
+++ 02_primitives/Challenge_3.cpp
@@ -1,3 +1,11 @@
+/* Favorite Tea Input Write a program that takes the user’s favorite tea as input using getline
+and also asks how many cups of tea they want using cin. Display the result in a fun message.
+
+Hint: Combine cin and getline carefully to avoid input issues.
+
+challenge1.md
+*/
+
 #include <iostream>
 #include <string>
 
